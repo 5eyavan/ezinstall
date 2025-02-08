cf7b982af584b7cc21308d1685030ef3385be3f2
Seyavan <130081209+5eyavan@users.noreply.github.com>
2025-02-08 21:22:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,10 @@ sudo apt install python3
 sudo apt install python3-pip
 cd ~
 curl -OL https://go.dev/dl/go1.23.6.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvf go1.16.7.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+source ~/.profile
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+go install -v github.com/projectdiscovery/notify/cmd/notify@latest
