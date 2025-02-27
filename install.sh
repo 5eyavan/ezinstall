31e1e914086ec57f7816c581f401986bfd0da353
Seyavan <130081209+5eyavan@users.noreply.github.com>
2025-02-28 02:33:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ cd ~
 curl -OL https://go.dev/dl/go1.23.6.linux-amd64.tar.gz
 sudo tar -C /usr/local -xvf go1.23.6.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+echo 'export PATH=$PATH:/home/{username}/go/bin' >> ~/.profile
 source ~/.profile
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
